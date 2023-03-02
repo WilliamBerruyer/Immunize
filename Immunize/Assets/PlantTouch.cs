e8d99c0cda018a1365bedf4e88bdde7a0e07af3b
hithahareendran <73360690+hithahareendran@users.noreply.github.com>
2023-03-02 16:28:01 +0100

    Entry Scene created
    Entry scene created with a c# script,  on touching the plant scene need to be changed to next scene.   if blood particles provide is should be attaching to the hand.

---
+++ Immunize/Assets/PlantTouch.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using OculusSampleFramework;
+
+public class PlantTouch : MonoBehaviour
+{
+    [SerializeField] public string nextScene; // Name of the next scene to load
+    [SerializeField] public ParticleSystem bloodParticles; // Reference to the blood particle system
+    public bool enableBlood;
+
+    /*private void Start()
+    {
+        Invoke("LoadNextScene", 3f);
+    }*/
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Check if the collider belongs to a hand
+        if (other.CompareTag("PlayerHand"))
+        {
+            if (enableBlood)
+            {
+                // Spawn blood particles at the location of the user's hand
+                SpawnBloodParticles(other.transform);
+
+                // Load the next scene after a delay
+                Invoke("LoadNextScene", 1.5f);
+            }
+            else
+            {
+                Invoke("LoadNextScene", 1.5f);
+            }
+        }
+    }
+
+
+    private void LoadNextScene()
+    {
+        SceneManager.LoadScene(nextScene); // Load the next scene
+       // SceneManager.UnloadSceneAsync("EntryScene"); 
+    }
+
+    private void SpawnBloodParticles(Transform handTransform)
+    {
+        // Instantiate the blood particle effect at the hand position
+        ParticleSystem bloodEffect = Instantiate(bloodParticles, handTransform.position, Quaternion.identity);
+
+        // Attach the blood particle effect to the hand transform as a child
+        bloodEffect.transform.SetParent(handTransform);
+
+        // Play the blood particle effect
+        bloodEffect.Play();
+    }
+}
